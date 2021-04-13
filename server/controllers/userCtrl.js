4f62de8d4c66fe96a4f9df2a32f4640ccf46831f
Yannick-Z <64752052+Yannick-Z@users.noreply.github.com>
2021-04-13 16:43:32 +0200

    up

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,14 +1,91 @@
 const modelUser = require("../models/userModel");
-async function register(username, password){
-        try{
-            await modelUser.register(username, password);
-            return true;
-    }
-    catch(e){
-        console.log(register)
+const db = require('../config/db');
+const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken');
 
+
+module.exports = {
+    
+    
+    register: (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    bcrypt.hash(password, 10, function(err, hash){
+        db.query(
+            "INSERT INTO Users (username, password, role) VALUES (?, ?, ?);",
+            [username, hash, 'user'],
+            (err, results) => {
+                console.log(err);
+                console.log (results);
+                res.send(results);
+            }
+        );
+    })
+    // db.query(
+    //     "INSERT INTO Users (username, password, role) VALUES (?, ?, ?);",
+    //     [username, password, 'user'],
+    //     (err, results) => {
+    //         console.log(err);
+    //         console.log (results);
+    //         res.send(results);
+    //     }
+    // );
+},
+
+login: (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(
+        "SELECT * FROM Users WHERE username = ?",
+    [username], 
+        (err, results) => {
+            
+            bcrypt.compare(password, results[0].password, function(err, result) {
+                if (err) {
+                    res.json({loggedIn: false, message: "Wrong username/password combo"});
+                    
+
+                } else {  
+                    res.json({
+                        loggedIn: true,
+                        username: username,
+                        password: password,
+                        id :  user._id,                  
+                        role : user.role,
+                        token: jwt.sign({ userId : user._id}, process.env.SECRET, {expiresIn: '24'})
+                        });
+                }
+            });
+        //     if(err){
+        //     console.log(err);
+        // }
+        // if (results.length > 0) {
+        //     if (password == results[0].password){
+        //         res.json({loggedIn: true, username: username});
+        //     } else {
+        //         res.json({loggedIn: false, message: "Wrong username/password combo"});
+        //     }
+                
+        // } else {
+           
+        //     res.json({loggedIn: false, message: "doesn't exist"});
+        // }
+        
+        }
+    
+    
+    );
+
+
+    },
+
+    loginWithSession: (req,res) => {
+        if (req.session.user) {
+            res.send({ loggedIn: true, user: req.session.user})
+
+        } else {
+            res.send({loggedIn: false});
+        }
     }
 }
-
-
-module.exports.register = register;
--- server/routes/User.js
+++ server/routes/User.js
@@ -1,52 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/userCtrl');
 
-const db = require('../config/db');
 
-router.post("/register", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    db.query(
-        "INSERT INTO Users (username, password) VALUES (?, ?);",
-        [username, password],
-        (err, results) => {
-            console.log (results);
-        }
-    );
-});
 
-router.get('/login', (req, res) => {
-    if (req.session.user) {
-        res.send({ loggedIn: true, user: req.session.user})
-    } else {
-        res.send({loggedIn:false});
-    }
-});
 
-router.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    db.query(
-        "SELECT * FROM Users WHERE username = ?",
-        username, 
-        (err, results) => {
-            if(err){
-            console.log(err);
-        }
-        if (results.length > 0) {
-            if (password == results[0].password){
-                res.json({loggedIn: true, username: username});
-            } else {
-                res.json({loggedIn: false, message: "Wrong username/password combo"});
-            }
-                
-        } else {
-           
-            res.json({loggedIn: false, message: "doesn't exist"});
-        }
-        
-        }
-    );
-});
+
+
+
+router.post("/register", userController.register);
+
+router.get('/login', userController.loginWithSession);
+
+router.post("/login", userController.login); 
 
 module.exports = router;
